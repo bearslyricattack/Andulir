7f030fe0fdb3c39141f12c3de857244c8b885b59
eterblue <Eterblue1@gmail.com>
2024-04-03 10:29:20 +0800

    实现嵌套List的随机测试用例的生成

--- src/main/java/org/andulir/generator/ListDataGenerator.java
+++ src/main/java/org/andulir/generator/ListDataGenerator.java
@@ -6,6 +6,8 @@ import org.dom4j.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.co.jemos.podam.api.PodamFactory;
+import java.lang.reflect.Type;
+
 
 @Component
 public class ListDataGenerator implements DataGenerator {
@@ -17,15 +19,30 @@ public class ListDataGenerator implements DataGenerator {
     public String generateRandomData(String typeName, Element typeMapping) {
         Class<?> listClass = null;
         Class<?> gClass = null;
+        String className=null;
+        Type actualType=null;
+        ParameterizedTypeImpl beforeType=null;
+        int count=0;
         try {
             listClass = Class.forName("java.util.List");
-
-            gClass = Class.forName(typeName.substring(typeName.indexOf('<') + 1, typeName.length() - 1));
+            className=typeName.substring(typeName.indexOf('<') + 1, typeName.length() - 1);
+            while(className.contains("java.util.List")){
+                count++;
+                className=className.substring(className.indexOf('<')+1,className.length()-1);
+            }
+            Type[] types=new Type[count+1];
+            for (int i = 0; i < count; i++) {
+                types[i]=listClass;
+            }
+            types[count]=Class.forName(className);
+            for (int i = types.length - 1; i > 0; i--) {
+                beforeType = new ParameterizedTypeImpl(new Type[]{beforeType == null ? types[i] : beforeType}, null, types[i - 1]);
+            }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        Object value = podamFactory.manufacturePojo(listClass, gClass);
+        Object value = podamFactory.manufacturePojo(listClass, beforeType);
         try {
             return objectMapper.writeValueAsString(value);
         } catch (JsonProcessingException e) {
